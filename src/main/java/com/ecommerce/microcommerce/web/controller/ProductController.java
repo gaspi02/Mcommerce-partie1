2796082791a119542d50026a31fe6a2c5209e750
Gaspi02 <duringer.gaspard@gmail.com>
2018-08-28 15:44:37 +0200

    Commit Validation du prix de vente
    Ajout exception

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -91,6 +92,8 @@ public class ProductController {
 
         if (productAdded == null)
             return ResponseEntity.noContent().build();
+
+        if(productAdded.getPrix() == 0) throw new ProduitGratuitException("Le produit est GRATUIT. Tu ferais un commerçant pitoyable.");
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
